81114b7c39875908cc1ab572a97e42be6d1cabb8
mplawley <mark.lawley@gmail.com>
2017-03-08 11:58:35 -0500

    Scene Controller script

---
+++ Assets/Scripts/Start menu/SceneController.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * This script controls the game flow from scene to scene
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+	public static SceneController instance; //singleton
+
+	void Awake()
+	{
+		//Singleton
+		if (instance == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			instance = this;
+		}
+		else if (instance != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	public void LoadScene(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+	}
+}
