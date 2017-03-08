d8be8b881ac6487064aec0dcbfe4d31ffdcfa483
mplawley <mark.lawley@gmail.com>
2017-03-08 12:30:51 -0500

    Memory manager script
    -To handle all data management in the game, persistent from scene to
    scene

---
+++ Assets/Scripts/Game/MemoryManager.cs
@@ -0,0 +1,128 @@
+﻿/*
+ * This script handles the game's memory and data management.
+ */
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class MemoryManager : MonoBehaviour
+{
+	[Header("Data settings")]
+	public static MemoryManager instance;
+	public AllGameData allGameDataOnSingleton = new AllGameData();
+
+	// Singleton
+	void Awake ()
+	{
+		if (instance == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			instance = this;
+		}
+		else if (instance != this)
+		{
+			Destroy (gameObject);
+		}
+	}
+
+/*
+ * *************************************************************************************************
+ * *************************************************************************************************
+ * *************************************** FUNDAMENTAL MEMORY MANAGEMENT ***************************
+ * ********************************************* FILE I/O ******************************************
+ * *************************************************************************************************
+ */
+
+	public void Save()
+	{
+		//Use a binary formatter to write
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Create(Application.persistentDataPath + "/DungeonGenerator" + "-" + allGameDataOnSingleton.dungeonData.gameVersion + ".dat");
+
+		//Write data from singleton to memory
+		AllGameData allGameDataToMemory = allGameDataOnSingleton;
+
+		//Serialize data
+		bf.Serialize(file, allGameDataToMemory);
+
+		//File I/O
+		file.Close();
+	}
+
+	public void Load()
+	{
+		if (File.Exists(Application.persistentDataPath + "/DungeonGenerator" + "-" + allGameDataOnSingleton.dungeonData.gameVersion + ".dat")) 
+		{
+			//Use binary formatter to read
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open(Application.persistentDataPath + "/DungeonGenerator" + "-" + allGameDataOnSingleton.dungeonData.gameVersion + ".dat", FileMode.Open);
+
+			//Deserialize data from memory
+			AllGameData allGameDataFromMemory = (AllGameData)bf.Deserialize(file);
+
+			//File I/O
+			file.Close();
+
+			//Write data from memory to singleton
+			allGameDataOnSingleton = allGameDataFromMemory;
+		}
+	}
+
+	//Clear ONLY currently loaded singleton data. Used when exiting a chapter.
+	public void ClearOnlySingletonData()
+	{
+		allGameDataOnSingleton.dungeonData.ClearData();
+		//TODO: If you ever add to this game, also clear, for instance, Chapter2Data, MarketData, etc.
+	}
+}
+
+/*
+ * *************************************************************************************************
+ * *************************************************************************************************
+ * *************************************** DATA ****************************************************
+ * *************************************************************************************************
+ * *************************************************************************************************
+ */
+
+[Serializable]
+public class DungeonGeneratorData
+{
+	//Version
+	public string characterToSaveLoad = "";
+	public string gameVersion = "1.00";
+
+	//Player
+	public int playerLife;
+	public int playerSpeed;
+	public int playerStrength;
+	public int playerWisdom;
+
+	//Later chapter completed. Defaults to 0 for none. 1 for chapter 1 completed, 10 for chapter 10 completed, etc.
+	public bool firstPlayThrough = false;
+
+	//Class methods
+	public void ClearData()
+	{
+		playerLife = 0;
+		playerSpeed = 0;
+		playerStrength = 0;
+		playerWisdom = 0;
+	}
+}
+
+[Serializable]
+public class AllGameData //I chose to have an AllGameData in hypothetical anticipation of needing, for instance, MarketData, Chapter2Data, etc.
+{
+	//Game 1
+	public DungeonGeneratorData dungeonData = new DungeonGeneratorData();
+
+	//Chapter 2
+	//TODO: For future building!
+
+	//Market
+	//TODO: If this app ever needed, for instance, in-app-purchases to be implemented....
+}
