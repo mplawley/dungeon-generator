5e8146d22a932cc29e6d731109dd6641ee107fb8
mplawley <mark.lawley@gmail.com>
2017-03-09 12:42:56 -0500

    Room transitions implemented!

--- Assets/Scripts/Game/RoomTransitionController.cs
+++ Assets/Scripts/Game/RoomTransitionController.cs
@@ -15,82 +15,59 @@ public class RoomTransitionController : MonoBehaviour
 	public Vector3 transitionVectorUp;
 	public Vector3 transitionVectorDown;
 
-	public Vector3 playerSouthPosition = new Vector3();
-	public Vector3 playerWestPosition;
-	public Vector3 playerEastPosition;
-	public Vector3 playerNorthPosition;
+	public Vector3 newPlayerPosition = new Vector3();
 	Vector3 newDungeonPosition;
 
-	private float step;
-	public float transitionSpeed;
+	private float step = 1.0f;
 
-	public bool touchedDoor = false;
-	
-	// Update is called once per frame
-	void FixedUpdate()
+
+
+	void OnTriggerEnter2D(Collider2D coll)
 	{
-		if (touchedDoor)
+		if (coll.gameObject.tag == "Door")
 		{
 			if (PlayerMotionController.instance.playerFacing == facing.up)
 			{
 				//Move dungeon....
 				newDungeonPosition = dungeonHolder.position - transitionVectorUp;
-				step = transitionSpeed * Time.deltaTime;
-				dungeonHolder.position = Vector3.MoveTowards(dungeonHolder.position, newDungeonPosition, step);
+				dungeonHolder.position = newDungeonPosition;
 
 				//Put player at bottom of room....
-				playerSouthPosition = new Vector3(PlayerMotionController.instance.boundary.yMin, gameObject.transform.position.y, 0f);
-				gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, playerSouthPosition, step);
+				newPlayerPosition = new Vector3(gameObject.transform.position.x, PlayerMotionController.instance.boundary.yMin, 0f);
+				gameObject.transform.position = newPlayerPosition;
 
 			}
 			else if (PlayerMotionController.instance.playerFacing == facing.right)
 			{
 				//Move dungeon....
 				newDungeonPosition = dungeonHolder.position - transitionVectorRight;
-				step = transitionSpeed * Time.deltaTime;
-				dungeonHolder.position = Vector3.MoveTowards(dungeonHolder.position, newDungeonPosition, step);
+				dungeonHolder.position = newDungeonPosition;
 
 				//Put player at left of room....
-				playerWestPosition = new Vector3(PlayerMotionController.instance.boundary.xMin, gameObject.transform.position.y, 0f);
-				gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, playerWestPosition, step);
+				newPlayerPosition = new Vector3(PlayerMotionController.instance.boundary.xMin, gameObject.transform.position.y, 0f);
+				gameObject.transform.position = newPlayerPosition;
 			}
 			else if (PlayerMotionController.instance.playerFacing == facing.down)
 			{
 				//Move dungeon....
 				newDungeonPosition = dungeonHolder.position - transitionVectorDown;
-				step = transitionSpeed * Time.deltaTime;
-				dungeonHolder.position = Vector3.MoveTowards(dungeonHolder.position, newDungeonPosition, step);
+				dungeonHolder.position = newDungeonPosition;
 
 				//Put player at top of room....
-				playerWestPosition = new Vector3(gameObject.transform.position.x, PlayerMotionController.instance.boundary.yMax, 0f);
-				gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, playerWestPosition, step);
+				newPlayerPosition = new Vector3(gameObject.transform.position.x, PlayerMotionController.instance.boundary.yMax, 0f);
+				gameObject.transform.position = newPlayerPosition;
 			}
 			else if (PlayerMotionController.instance.playerFacing == facing.left)
 			{
 				//Move dungeon....
 				newDungeonPosition = dungeonHolder.position - transitionVectorLeft;
-				step = transitionSpeed * Time.deltaTime;
-				dungeonHolder.position = Vector3.MoveTowards(dungeonHolder.position, newDungeonPosition, step);
+				dungeonHolder.position = newDungeonPosition;
 
 				//Put player at right of room....
-				playerWestPosition = new Vector3(PlayerMotionController.instance.boundary.xMax, gameObject.transform.position.y, 0f);
-				gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, playerWestPosition, step);
+				newPlayerPosition = new Vector3(PlayerMotionController.instance.boundary.xMax, gameObject.transform.position.y, 0f);
+				gameObject.transform.position = newPlayerPosition;
 			}
-			Invoke("GrantPlayerMovement", 5.0f);
-		}
-	}
 
-	void OnTriggerEnter2D(Collider2D coll)
-	{
-		if (coll.gameObject.tag == "Door")
-		{
-			touchedDoor = true;
 		}
-	}
-
-	public void GrantPlayerMovement()
-	{
-		gameObject.GetComponent<PlayerMotionController>().enabled = true;
-		touchedDoor = false;
 	}
 }
