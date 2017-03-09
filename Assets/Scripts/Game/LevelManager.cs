666332d9be1aa444bdb1ab8c06d1eee48b86491a
mplawley <mark.lawley@gmail.com>
2017-03-08 19:40:00 -0500

    Prefabs for entire rooms vs tiles

--- Assets/Scripts/Game/LevelManager.cs
+++ Assets/Scripts/Game/LevelManager.cs
@@ -1,16 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class LevelManager : MonoBehaviour {
+public class LevelManager : MonoBehaviour
+{
+	[Header("Procedural generation fields")]
+	int maxRooms;
+	int maxCorridors;
+	int maxRoomWidth = 16;
+	int maxRoomHeight = 8;
+
+	[Header("General terrain")]
+	[SerializeField]
+	List<GameObject> generalRoomPrefabs = new List<GameObject>();
+
+	[SerializeField]
+	List<GameObject> specialRoomPrefabs = new List<GameObject>();
+
+	[SerializeField]
+	List<GameObject> groundTiles = new List<GameObject>();
+
+	[SerializeField]
+	List<GameObject> outerWalls = new List<GameObject>();
+
+	[SerializeField]
+	List<GameObject> trapTiles = new List<GameObject>();
+
+	[Header("Doors")]
+	List<GameObject> doorTiles = new List<GameObject>();
+
+
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+		//Place the first room....
+		PlaceFirstRoom();
+
+		//GenerateDungeon();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	void PlaceFirstRoom()
+	{
+		//Generate random number....
+		int randomNumber = Random.Range(0, generalRoomPrefabs.Count);
+	}
+
+	void GenerateDungeon()
+	{
 		
+		for (int i = 0; i < maxRoomWidth; i++)
+		{
+			//	
+		}
 	}
 }
